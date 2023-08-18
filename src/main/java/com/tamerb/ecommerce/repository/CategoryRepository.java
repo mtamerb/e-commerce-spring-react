2b588ad686d01d3c8bef0c5523732027569db87f
tamer bilici <tamerbilici00@gmail.com>
2023-08-18 21:47:15 +0300

    repository

---
+++ src/main/java/com/tamerb/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.tamerb.ecommerce.repository;
+
+import com.tamerb.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
+
+
