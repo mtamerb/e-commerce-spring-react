1d9c759b25af6fe0ba308354d5d80a3fdc9a396e
tamer bilici <tamerbilici00@gmail.com>
2023-08-18 22:29:34 +0300

    controller

---
+++ src/main/java/com/tamerb/ecommerce/controllers/CategoryController.java
@@ -0,0 +1,16 @@
+package com.tamerb.ecommerce.controllers;
+
+import com.tamerb.ecommerce.service.CategoryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+}
