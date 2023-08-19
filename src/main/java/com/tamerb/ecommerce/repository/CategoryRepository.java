66328c913c1867c8168e75d5d95922ba6096b61a
tamer bilici <tamerbilici00@gmail.com>
2023-08-19 20:53:20 +0300

    repository

--- src/main/java/com/tamerb/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/tamerb/ecommerce/repository/CategoryRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+    Category findByCategoryName(String categoryName);
 }
 
 
