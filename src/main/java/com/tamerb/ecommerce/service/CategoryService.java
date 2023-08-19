607fc4c2daf14bc4832bf6576cf84f86cb67b788
tamer bilici <tamerbilici00@gmail.com>
2023-08-19 20:52:58 +0300

    service

--- src/main/java/com/tamerb/ecommerce/service/CategoryService.java
+++ src/main/java/com/tamerb/ecommerce/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.tamerb.ecommerce.service;
 
+import com.tamerb.ecommerce.model.Category;
 import com.tamerb.ecommerce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,13 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public Category readCategory(String categoryName) {
+        return categoryRepository.findByCategoryName(categoryName);
+    }
+
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
     }
 }
