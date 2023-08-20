d5ee9f81d51bfd1a6e8b2f400b21a12fbf47ae12
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 01:59:13 +0300

    swagger

---
+++ src/main/java/com/tamerb/ecommerce/config/common/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.tamerb.ecommerce.config.common;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springEcommerceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("E-Commerce API")
+                        .description("E commerce app sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("e-Commerce Wiki Documentation")
+                        .url("https://github.com/mtamerb"));
+    }
+
+
+
+
+
+}
+
