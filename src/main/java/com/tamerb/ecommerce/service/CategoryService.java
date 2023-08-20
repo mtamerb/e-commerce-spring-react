99ab4cc495316974e3945e8c1294a281f2b22510
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 02:18:59 +0300

    list categories

--- src/main/java/com/tamerb/ecommerce/service/CategoryService.java
+++ src/main/java/com/tamerb/ecommerce/service/CategoryService.java
@@ -5,23 +5,26 @@ import com.tamerb.ecommerce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
     public CategoryService(CategoryRepository categoryRepository) {
-
         this.categoryRepository = categoryRepository;
     }
 
     public Category readCategory(String categoryName) {
-
         return categoryRepository.findByCategoryName(categoryName);
     }
 
     public void createCategory(Category category) {
-
         categoryRepository.save(category);
+    }
+
+    public List<Category> listCategories() {
+        return categoryRepository.findAll();
     }
 }
