3dbf56c6b619be520867b86b9dd0eac13614141e
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 02:32:27 +0300

    update and read categories

--- src/main/java/com/tamerb/ecommerce/service/CategoryService.java
+++ src/main/java/com/tamerb/ecommerce/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -26,5 +27,17 @@ public class CategoryService {
 
     public List<Category> listCategories() {
         return categoryRepository.findAll();
+    }
+
+    public Optional<Category> readCategory(Integer categoryID) {
+        return categoryRepository.findById(categoryID);
+    }
+
+    public void updateCategory(Integer categoryID, Category newCategory) {
+        Category category = categoryRepository.findById(categoryID).get();
+        category.setCategoryName(newCategory.getCategoryName());
+        category.setDescription(newCategory.getDescription());
+        category.setImageUrl(newCategory.getImageUrl());
+        categoryRepository.save(category);
     }
 }
