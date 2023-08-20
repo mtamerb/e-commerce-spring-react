fd586b01a256ffdb127251008f8cbe2af4540544
tamer bilici <tamerbilici00@gmail.com>
2023-08-20 22:17:14 +0300

    config

--- src/main/java/com/tamerb/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/tamerb/ecommerce/config/SwaggerConfig.java
@@ -1,5 +1,42 @@
 package com.tamerb.ecommerce.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket ecommerceApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.tamerb.ecommerce"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("e commmerce ", "https://github.com/mtamerb", "tamerbilicixyz.com");
+        return new ApiInfoBuilder()
+                .title("Ecommerce API")
+                .description("Documentation Ecommerce api")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
+    }
 
 }
