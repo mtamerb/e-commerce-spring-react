8d80e1752e5fc2a916630902dc00621f333dd3fb
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 20:28:41 +0300

    product repository

---
+++ src/main/java/com/tamerb/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tamerb.ecommerce.repository;
+
+import com.tamerb.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
