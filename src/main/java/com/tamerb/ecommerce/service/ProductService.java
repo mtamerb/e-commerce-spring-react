dbd9ed8ff8845f763263d8fa0065e8fce241134a
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 20:32:55 +0300

    product service

---
+++ src/main/java/com/tamerb/ecommerce/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.tamerb.ecommerce.service;
+
+import com.tamerb.ecommerce.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
