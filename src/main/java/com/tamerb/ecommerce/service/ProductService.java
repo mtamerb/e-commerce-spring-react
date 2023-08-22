e82e772651192609e0c06dfd13ad4dd5c885e8b5
tamer bilici <tamerbilici00@gmail.com>
2023-08-22 19:35:50 +0300

    update req

--- src/main/java/com/tamerb/ecommerce/service/ProductService.java
+++ src/main/java/com/tamerb/ecommerce/service/ProductService.java
@@ -18,22 +18,6 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public void addProduct(ProductDto productDto, Category category) {
-        Product product = getProductFromDto(productDto, category);
-        productRepository.save(product);
-    }
-
-
-    public List<ProductDto> listProducts() {
-        List<Product> products = productRepository.findAll();
-        List<ProductDto> productDtos = new ArrayList<>();
-
-        for(Product product : products) {
-            productDtos.add(new ProductDto(product));
-        }
-        return productDtos;
-    }
-
     private Product getProductFromDto(ProductDto productDto, Category category) {
         Product product = new Product();
         product.setName(productDto.getName());
@@ -44,4 +28,24 @@ public class ProductService {
         return product;
     }
 
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    public List<ProductDto> listProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+
+        for (Product product : products) {
+            productDtos.add(new ProductDto(product));
+        }
+        return productDtos;
+    }
+
+    public void updateProduct(Integer productId, ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        product.setId(productId);
+        productRepository.save(product);
+    }
 }
