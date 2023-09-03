c706b0a2030185d0b31cd3322dd03dfbf660091b
hamza yıldırım <hmz607015@gmail.com>
2023-09-03 13:52:51 +0300

    register componenti eklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Registor from "./components/Register";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Registor />
+    </>
+  );
 }
 
 export default App;
