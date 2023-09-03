1e2f243040d1925b9d6dadbac42afd04bfa7a6a1
hamza yıldırım <hmz607015@gmail.com>
2023-09-03 16:53:19 +0300

    signin ve login sayfaları arası geçiş için component eklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./components/Register.jsx";
 import Signin from "./components/Signin.jsx";
+import Routebuttons from "./components/Routebuttons.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Routebuttons />
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/login" element={<Register />} />
@@ -12,5 +14,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
