e9e8625905c255eabf5169d05e97e4ac1b56a209
hamza yıldırım <hmz607015@gmail.com>
2023-09-03 20:44:37 +0300

    admin butonu

--- frontend/src/components/Routebuttons.jsx
+++ frontend/src/components/Routebuttons.jsx
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom";
 function Routebuttons() {
   console.log("rendered");
   return (
-    <div className="">
-      <button type="button" className="btn bg-light m-2 ">
-        <Link to="/" className="text-decoration-none">
-          Sign in
-        </Link>
-      </button>
+    <div className=" d-flex">
+      <div>
+        <button type="button" className="btn bg-light m-2 ">
+          <Link to="/" className="text-decoration-none">
+            Sign in
+          </Link>
+        </button>
+
+        <button type="button" className="btn bg-light m-2 ">
+          <Link to="/register" className="text-decoration-none">
+            Register
+          </Link>
+        </button>
+      </div>
 
       <button type="button" className="btn bg-light m-2 ">
-        <Link to="/register" className="text-decoration-none">
-          Register
+        <Link to="/admin" className="text-decoration-none">
+          Admin
         </Link>
       </button>
     </div>
