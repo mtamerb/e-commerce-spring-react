391867e787f64ea03e6c673177908957f968c9ee
hamza yıldırım <hmz607015@gmail.com>
2023-09-03 16:07:35 +0300

    route işlemi için ap jsxe import işlemi yapıldı ve route yolları belirlenmeye başlandı

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./components/Register.jsx";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Register />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
