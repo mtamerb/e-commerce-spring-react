60519f93da007debb80bcd84d2e58eddc18d2853
hamza yıldırım <hmz607015@gmail.com>
2023-09-03 16:31:52 +0300

    sign in componenti eklendi ve route kısmı ayarlandı

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./components/Register.jsx";
+import Signin from "./components/Signin.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="/login" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
