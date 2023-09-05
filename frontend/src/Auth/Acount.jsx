e2b718d2c9235cc9562c53ba2c87a570f5d2d07f
hamza yıldırım <hmz607015@gmail.com>
2023-09-05 12:14:11 +0300

    acount componenti oluşturuldu

---
+++ frontend/src/Auth/Acount.jsx
@@ -0,0 +1,20 @@
+function Acount() {
+  return (
+    <div className="btn group">
+      <button
+        type="button"
+        className="btn  m-2  text-info"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Acount
+      </button>
+      <ul className="dropdown-menu text-info">
+        <li>profile</li>
+        <li>basket</li>
+        <li>settings</li>
+      </ul>
+    </div>
+  );
+}
+export default Acount;
