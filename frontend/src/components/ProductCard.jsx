e210353eb3bf3cc723978638a4649094c59c964a
hamza yıldırım <hmz607015@gmail.com>
2023-09-07 22:31:23 +0300

    bazı front endd işleleri

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,11 +1,12 @@
-function ProductCard(item) {
+function ProductCard({ product }) {
+  console.log(product.avatar);
   return (
     <div className="card m-4" style={{ width: "20rem" }}>
-      <img src={item.avatar} className="card-img-top" alt="..." />
+      <img src={product.imageUrl} className="w-100 h-50" alt="..." />
       <div className="card-body"></div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">{product.name}</li>
+        <li className="list-group-item">{product.price}$</li>
       </ul>
       <div className="card-body d-flex justify-content-between">
         <button type="button" className="btn btn-success ">
