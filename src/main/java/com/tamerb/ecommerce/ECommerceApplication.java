eba40493998821e6b11b677c1ed1bc29c78f1177
tamer bilici <tamerbilici00@gmail.com>
2023-09-07 03:07:16 +0300

    clean

--- src/main/java/com/tamerb/ecommerce/ECommerceApplication.java
+++ src/main/java/com/tamerb/ecommerce/ECommerceApplication.java
@@ -2,8 +2,7 @@ package com.tamerb.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+
 
 @SpringBootApplication
 public class ECommerceApplication {
