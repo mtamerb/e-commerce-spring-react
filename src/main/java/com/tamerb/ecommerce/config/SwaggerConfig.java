a2c3de9e05a72cf80fa7dff8bfc65a2492c12b0c
tamer bilici <tamerbilici00@gmail.com>
2023-09-16 14:27:37 +0300

    swagger

--- src/main/java/com/tamerb/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/tamerb/ecommerce/config/SwaggerConfig.java
@@ -1,25 +1,51 @@
 package com.tamerb.ecommerce.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@Configuration
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Tamer Bilici",
+                        email = "tamerbilici00@gmail.com",
+                        url = "https://github.com/mtamerb"
+                ),
+                description = "This is a sample Spring Boot RESTful service using Springdoc OpenAPI and Swagger UI.",
+                title = "E-Commerce API",
+                version = "1.0.0",
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                ),
+                termsOfService = "http://swagger.io/terms/"
+        ),
+        servers = {
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        description = "Local Server",
+                        url = "http://localhost:8080"
+                )/*,
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        description = "Heroku Server",
+                        url = "https://ecommerce-springboot.herokuapp.com"
+                )*/
+        }
+)
+/*
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        description = "JWT Authorization header using the Bearer scheme.",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+*/
+
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI springEcommerceOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("E-Commerce API")
-                        .description("E commerce app sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("e-Commerce repository")
-                        .url("https://github.com/mtamerb"));
-    }
 }
-
