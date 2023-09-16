84636be9b4240b1d468a99e47522e671716953af
tamer bilici <tamerbilici00@gmail.com>
2023-09-16 22:37:23 +0300

    delete method and operation annotation added

--- src/main/java/com/tamerb/ecommerce/controllers/CategoryController.java
+++ src/main/java/com/tamerb/ecommerce/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package com.tamerb.ecommerce.controllers;
 import com.tamerb.ecommerce.config.ApiResponse;
 import com.tamerb.ecommerce.entities.Category;
 import com.tamerb.ecommerce.business.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,8 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @PostMapping("/create")
+    @Operation(summary = "Create a category")
+    @PostMapping("")
     public ResponseEntity<ApiResponse> createCategory(@Valid @RequestBody Category category) {
         if (Objects.nonNull(categoryService.readCategory(category.getCategoryName()))) {
             return new ResponseEntity<>(new ApiResponse(false, "Category already exists"), HttpStatus.CONFLICT);
@@ -30,21 +32,41 @@ public class CategoryController {
         return new ResponseEntity<>(new ApiResponse(true, "Category created successfully"), HttpStatus.CREATED);
     }
 
-    @GetMapping("/list")
+    @Operation(summary = "Get all categories")
+    @GetMapping("")
     public ResponseEntity<List<Category>> getCategories() {
         List<Category> body = categoryService.listCategories();
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
-    @PostMapping("/update/{categoryID}")
+    @Operation(summary = "Update a category by ID")
+    @PutMapping("/{categoryID}")
     public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryID") Long categoryID, @Valid @RequestBody Category category) {
         if (Objects.nonNull(categoryService.readCategory(categoryID))) {
             categoryService.updateCategory(categoryID, category);
             return new ResponseEntity<>(new ApiResponse(true, "Category updated successfully"), HttpStatus.OK);
         }
         return new ResponseEntity<>(new ApiResponse(false, "Category not found"), HttpStatus.NOT_FOUND);
+    }
 
+    @Operation(summary = "Delete a category by ID")
+    @DeleteMapping("/{categoryID}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable("categoryID") Long categoryID){
+        if(Objects.nonNull(categoryService.readCategory(categoryID))){
+            categoryService.deleteCategory(categoryID);
+            return new ResponseEntity<>(new ApiResponse(true, "Category deleted successfully"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ApiResponse(false, "Category not found"), HttpStatus.NOT_FOUND);
     }
 
 
+    @Operation(summary = "Delete all categories")
+    @DeleteMapping("")
+    public ResponseEntity<ApiResponse> deleteAll(){
+        if(Objects.nonNull(categoryService.listCategories())){
+            categoryService.deleteAllCategory();
+            return new ResponseEntity<>(new ApiResponse(true, "Category deleted successfully"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ApiResponse(false, "Categories empty"), HttpStatus.NOT_FOUND);
+    }
 }
