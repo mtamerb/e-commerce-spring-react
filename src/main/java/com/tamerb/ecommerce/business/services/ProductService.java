ba1538c868ea97bcd76e4d7c814cc64e8f761f34
tamer bilici <tamerbilici00@gmail.com>
2023-09-16 23:10:04 +0300

    product methods added

--- src/main/java/com/tamerb/ecommerce/business/services/ProductService.java
+++ src/main/java/com/tamerb/ecommerce/business/services/ProductService.java
@@ -18,34 +18,37 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    private Product getProductFromDto(ProductDto productDto, Category category) {
-        Product product = new Product();
-        product.setName(productDto.getName());
-        product.setImageUrl(productDto.getImageURL());
-        product.setPrice(productDto.getPrice());
-        product.setCategory(category);
-        product.setDescription(productDto.getDescription());
-        return product;
-    }
 
-    public void addProduct(ProductDto productDto, Category category) {
-        Product product = getProductFromDto(productDto, category);
-        productRepository.save(product);
-    }
 
     public List<ProductDto> listProducts() {
         List<Product> products = productRepository.findAll();
         List<ProductDto> productDtos = new ArrayList<>();
-
         for (Product product : products) {
             productDtos.add(new ProductDto(product));
         }
         return productDtos;
     }
 
-    public void updateProduct(Long id, ProductDto productDto, Category category) {
-        Product product = getProductFromDto(productDto, category);
-        product.setId(id);
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public void deleteAllProduct() {
+        productRepository.deleteAll();
+    }
+
+    public Object readProduct(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+
+    public void createProduct(ProductDto productDto, Category category) {
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setImageUrl(productDto.getImageURL());
+        product.setCategory(category);
         productRepository.save(product);
     }
 }
