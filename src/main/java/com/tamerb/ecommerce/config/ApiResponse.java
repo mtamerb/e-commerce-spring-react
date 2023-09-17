a23671d99b1b281cc79c728da633271fd6fc153b
tamer bilici <tamerbilici00@gmail.com>
2023-09-17 10:34:35 +0300

    converted to record

--- src/main/java/com/tamerb/ecommerce/config/ApiResponse.java
+++ src/main/java/com/tamerb/ecommerce/config/ApiResponse.java
@@ -2,24 +2,8 @@ package com.tamerb.ecommerce.config;
 
 import java.time.LocalDateTime;
 
-public class ApiResponse {
+public record ApiResponse(boolean success, String message) {
 
-    private final String message;
-    private final boolean success;
-
-    public ApiResponse(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 
     public String getTimestamp() {
         return LocalDateTime.now().toString();
